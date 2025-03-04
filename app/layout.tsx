9ae44143e13353d97698e9f0fb665354ca924bd1
Jakub Gania <contact@jakubgania.io>
2025-03-04 23:47:36 +0100

    development

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,17 +3,6 @@ import type { Metadata } from "next";
 import { Inter } from 'next/font/google';
 import "./globals.css";
 
-// const geistSans = localFont({
-//   src: "./fonts/GeistVF.woff",
-//   variable: "--font-geist-sans",
-//   weight: "100 900",
-// });
-// const geistMono = localFont({
-//   src: "./fonts/GeistMonoVF.woff",
-//   variable: "--font-geist-mono",
-//   weight: "100 900",
-// });
-
 const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
@@ -28,7 +17,6 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" className={inter.className}>
-    {/* <html lang="en" className={geistSans.className}> */}
       <body
         className="antialiased bg-white"
       >
